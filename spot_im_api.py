174f0a57b51c62cf96418933846d89090d7d2af7
tim <tim@luminosuslight.de>
2017-10-30 15:58:24 +0100

    Fixed issues woth comment crawling.

--- spot_im_api.py
+++ spot_im_api.py
@@ -4,35 +4,6 @@ import json
 import pprint
 
 
-# def get_rt_comments(article_url):
-#     spotim_spotId = 'sp_6phY2k0C' # spotim id for RT
-#     post_id = re.search('([0-9]+)', article_url).group(0)
-#
-#     # x = requests.post('https://api.spot.im/me/network-token/spotim')
-#     # print(x)
-#     # r1 = x.json()
-#     spotim_token = "01171025gwZEnP"
-#
-#     payload = {
-#         "count": 25, #number of comments to fetch
-#         "sort_by":"best",
-#         "cursor":{"offset":0,"comments_read":0},
-#         "host_url": article_url,
-#         "canonical_url": article_url
-#     }
-#
-#     y = "https://open-api.spot.im/v1/spot-conversation-events/text-only?token=" + spotim_token + "&spot_id=" + spotim_spotId + "&post_id=406881&etag=0&count=2"
-#
-#     r2_url ='https://api.spot.im/conversation-read/spot/' + spotim_spotId + '/post/'+ post_id +'/get'
-#     r2 = requests.post(r2_url, data=json.dumps(payload), headers={'X-Spotim-Token': spotim_token , "Content-Type": "application/json"})
-#
-#     print(requests.get(y).json())
-#
-#     print(r2)
-#
-#     return r2.json()
-
-
 def get_comments_of_news_as_json(news_id):
     # rt.com uses Spot.IM for the comment section
     # this message reads a JSON string directly from the corresponding Spot.IM page
@@ -44,15 +15,69 @@ def get_comments_of_news_as_json(news_id):
     regex_result = regex_for_json.search(html)
     json_raw = regex_result.group(1)
     json_raw = json_raw.replace('\\"', '"')
+    json_raw = json_raw.replace('\\"', '"')
+
+    print(json_raw[5000:5110])
+
     comments_in_spot_im_format = json.loads(json_raw)
 
     # print for debugging
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(comments_in_spot_im_format["conversations"][0]["conversation"]["comments"])
+    print("--------------------------------")
+
+    return comments_in_spot_im_format
+
+
+def get_list_of_comments(comments_in_spot_im_format, url):
+    comment_part = comments_in_spot_im_format["conversations"][0]["conversation"]["comments"]
+
+    # comment_id, article_url, comment_author, comment_text, timestamp, parent_comment_id
+
+    comments = []
+
+    for root_comment in comment_part:
+        comment = dict()
+        comment["id"] = root_comment['id']
+        comment["url"] = url
+        comment["author"] = root_comment['user']['username']
+        comment["text"] = root_comment['entities'][0]['text']
+        comment["timestamp"] = root_comment['postedAt']
+        comment["parent_id"] = ""
+        comment["down_votes"] = root_comment['downVotesCount']
+        comment["up_votes"] = root_comment['upVotesCount']
+        comments.append(comment)
+
+        for reply in root_comment['replies']:
+            if reply.get('isDeleted', False):
+                continue
+
+            comment = dict()
+            comment["id"] = reply['id']
+            comment["url"] = url
+            comment["author"] = reply['user']['username']
+            comment["text"] = reply['entities'][0]['text']
+            comment["timestamp"] = reply['postedAt']
+            comment["parent_id"] = reply['rootMessageId']
+            comment["down_votes"] = reply['downVotesCount']
+            comment["up_votes"] = reply['upVotesCount']
+            comments.append(comment)
+
+        # print:
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(root_comment)
+        print("--------------------------------")
+
+    return comments
+
 
 
 if __name__ == '__main__':
     # news from 16 oct:
     # 'http://www.rt.com/usa/406881-trump-says-total-termination-of-iran-deal-possible/'
     # other news id that works: 407028
-    get_comments_of_news_as_json(407028)
+    c = get_comments_of_news_as_json(406881)
+    comments = get_list_of_comments(c, "http://www.rt.com/usa/406881-trump-says-total-termination-of-iran-deal-possible/")
+
+    for com in comments:
+        print(com)
